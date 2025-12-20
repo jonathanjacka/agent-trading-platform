610ac1d3febcafd3fbe3e4df3f7115e3ce0a8890
Jonathan Jacka <jonathanjacka@gmail.com>
2025-12-20 02:05:24 +0000

    Fixes rate limits

--- client/src/hooks/usePortfolioHistory.ts
+++ client/src/hooks/usePortfolioHistory.ts
@@ -5,6 +5,7 @@ export const usePortfolioHistory = (traderName: string, limit: number = 50) => {
   return useQuery({
     queryKey: ['portfolioHistory', traderName, limit],
     queryFn: () => tradersApi.getPortfolioHistory(traderName, limit),
+    staleTime: 5 * 60 * 1000, // 5 minutes - historical data doesn't change often
     enabled: !!traderName,
   });
 };
